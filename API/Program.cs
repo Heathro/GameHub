0be3d64e1847113013831b3dbb4524a64bd35ca7
Andrei Gerasjov <andrei.gerasjov@gmail.com>
2023-10-03 13:12:00 +0100

    Angular added

--- API/Program.cs
+++ API/Program.cs
@@ -8,12 +8,11 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
