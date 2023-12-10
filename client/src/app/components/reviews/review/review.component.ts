d8f8d7a74a4212c94f0a06d20bd30534cb44c899
Andrei Gerasjov <andrei.gerasjov@gmail.com>
2023-12-10 00:28:16 +0000

    Review timeago

--- client/src/app/components/reviews/review/review.component.ts
+++ client/src/app/components/reviews/review/review.component.ts
@@ -13,26 +13,4 @@ export class ReviewComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-  getDateFormat() {
-    if (!this.review) return;
-
-    const currentDate = new Date();
-    const currentDay = currentDate.toDateString();
-    const currentYear = currentDate.getFullYear();
-
-    const sentDate = new Date(this.review.reviewPosted);
-    const sentDay = sentDate.toDateString();
-    const sentYear = sentDate.getFullYear();
-
-    if (sentYear < currentYear) {
-      return 'd MMM yyyy, H:mm';
-    }
-    else if (sentDay === currentDay) {
-      return "'Today', H:mm'";
-    }
-    else {
-      return 'd MMM, H:mm';
-    }
-  }
 }
