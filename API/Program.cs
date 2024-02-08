337eb32f3c879bb53f6fd541c4918f15739f94da
Andrei Gerasjov <andrei.gerasjov@gmail.com>
2024-02-08 16:32:36 +0000

    Pre deploy

--- API/Program.cs
+++ API/Program.cs
@@ -19,27 +19,28 @@ var adminPassword = "";
 if (builder.Environment.IsDevelopment())
 {
     connString = builder.Configuration.GetConnectionString("DefaultConnection");
-
     adminPassword = builder.Configuration["AdminPassword"];
-}    
+}
 else 
 {
-    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+    // var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
  
-    connUrl = connUrl.Replace("postgres://", string.Empty);
-    var pgUserPass = connUrl.Split("@")[0];
-    var pgHostPortDb = connUrl.Split("@")[1];
-    var pgHostPort = pgHostPortDb.Split("/")[0];
-    var pgDb = pgHostPortDb.Split("/")[1];
-    var pgUser = pgUserPass.Split(":")[0];
-    var pgPass = pgUserPass.Split(":")[1];
-    var pgHost = pgHostPort.Split(":")[0];
-    var pgPort = pgHostPort.Split(":")[1];
-    var updatedHost = pgHost.Replace("flycast", "internal");
+    // connUrl = connUrl.Replace("postgres://", string.Empty);
+    // var pgUserPass = connUrl.Split("@")[0];
+    // var pgHostPortDb = connUrl.Split("@")[1];
+    // var pgHostPort = pgHostPortDb.Split("/")[0];
+    // var pgDb = pgHostPortDb.Split("/")[1];
+    // var pgUser = pgUserPass.Split(":")[0];
+    // var pgPass = pgUserPass.Split(":")[1];
+    // var pgHost = pgHostPort.Split(":")[0];
+    // var pgPort = pgHostPort.Split(":")[1];
+    // var updatedHost = pgHost.Replace("flycast", "internal");
  
-    connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+    // connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+    connString = builder.Configuration.GetConnectionString("DefaultConnection");
 
     adminPassword = Environment.GetEnvironmentVariable("AdminPassword");
+    //adminPassword = builder.Configuration["AdminPassword"];
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
